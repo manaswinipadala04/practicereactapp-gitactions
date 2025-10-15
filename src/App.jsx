a141d35a6d4e2010fb25bcd0698b8212e6b2058f
Manaswini <manaswinipadala04@gmail.com>
2025-10-15 19:42:13 +0530

    Project V1.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S105</h1>
+      <h1>Welcome to My React Practice Project</h1>
       <h1>CI/CD and Cloud DevOps Course</h1>
-      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>This is a Practice homepage deployed on GitHub Actions using Vite.</p>
       <h1>Done with Git Pages & Git Actions</h1>
     </div>
   );
